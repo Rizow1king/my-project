b30ced33240633b9228c428e3445820b1ee1718d
muhammadrizo <mt936425155@gmail.com>
2024-11-04 14:11:23 +0500

    javascript array

---
+++ Array.js
@@ -0,0 +1,28 @@
+// 1-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// console.log(fruits);
+// 2-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// fruits.push("Apelsin");
+// console.log(fruits);
+// 3-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// fruits.unshift("Banan"); 
+// console.log(fruits);
+// 4-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// fruits.push("Apelsin");
+// console.log(fruits);
+// 5-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// let deleted = fruits.pop();
+// console.log(fruits);
+// console.log(`Ro'yxatdan olib tashlangan so'z: ${deleted}`);
+// 6-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// let deleted = fruits.shift();
+// console.log(fruits);
+// console.log(`Ro'yxatdan olib tashlangan so'z: ${deleted}`);
+// 7-chi vazifa
+// const fruits = ["Apple", "Peach", "Orange"];
+// console.log(fruits.length);
